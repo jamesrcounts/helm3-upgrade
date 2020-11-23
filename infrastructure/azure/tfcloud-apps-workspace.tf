a395f6b466142c9c9a971008110c81578a9546bf
Jim Counts <jamesrcounts@outlook.com>
2020-11-23 14:50:53 +0000

    WIP, removing child workspace

--- infrastructure/azure/tfcloud-apps-workspace.tf
+++ infrastructure/azure/tfcloud-apps-workspace.tf
@@ -11,14 +11,7 @@ resource "tfe_variable" "arm_client_id" {
   workspace_id = tfe_workspace.apps.id
 }
 
-resource "tfe_variable" "arm_client_secret" {
-  category     = "env"
-  description  = "Service Principal Password"
-  key          = "ARM_CLIENT_SECRET"
-  sensitive    = true
-  value        = azuread_application_password.apps_password.value
-  workspace_id = tfe_workspace.apps.id
-}
+
 
 resource "tfe_variable" "arm_subscription_id" {
   category     = "env"
@@ -38,17 +31,8 @@ resource "tfe_variable" "arm_tenant_id" {
 
 data "azurerm_client_config" "current" {}
 
-resource "azuread_application_password" "apps_password" {
-  application_object_id = data.azuread_application.sp.id
-  description           = "Workspace: ${tfe_workspace.apps.id}"
-  end_date_relative     = "240h"
-  key_id                = random_uuid.apps_password_id.result
-  value                 = random_password.password.result
-}
 
-data "azuread_application" "sp" {
-  application_id = data.azurerm_client_config.current.client_id
-}
+
 
 resource "random_password" "password" {
   length           = 16
